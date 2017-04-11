fed4704e39ae3f5f615e9368b843d87db6562517
Syed Haider Hassan <pakistanihaider@gmail.com>
2017-04-11 12:48:36 -0700

    Just Updated the Menus on the Backend.

---
+++ resources/views/admin/components/nav/sidebar-menu.blade.php
@@ -0,0 +1,68 @@
+<ul class="sidebar-menu">
+    <li class="header">MAIN NAVIGATION</li>
+    <li class="active treeview">
+        <a href="#">
+            <i class="fa fa-users"></i> <span>Manage</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i>Users</a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i>Groups</a></li>
+        </ul>
+    </li>
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-book"></i>
+            <span>Resume</span>
+            <span class="pull-right-container">
+              <span class="label label-primary pull-right">4</span>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+            <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Basic </a></li>
+            <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Skills </a></li>
+            <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> What I Do?</a></li>
+            <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Portfolio</a></li>
+            <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Interests</a></li>
+            <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Experience</a></li>
+            <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Education</a></li>
+            <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Testimonials</a></li>
+            <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Pricing</a></li>
+            <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Contact</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="pages/widgets.html">
+            <i class="fa fa-th"></i> <span>Blog</span>
+            <span class="pull-right-container">
+              <small class="label pull-right bg-green">All</small>
+              <small class="label pull-right bg-green">Add New Blog</small>
+            </span>
+        </a>
+    </li>
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-cogs"></i>
+            <span>Configuration</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+            <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> System </a></li>
+            <li><a href="pages/charts/morris.html"><i class="fa fa-circle-o"></i> User </a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="pages/mailbox/mailbox.html">
+            <i class="fa fa-envelope"></i> <span>Mailbox</span>
+            <span class="pull-right-container">
+              <small class="label pull-right bg-yellow">12</small>
+              <small class="label pull-right bg-green">16</small>
+              <small class="label pull-right bg-red">5</small>
+            </span>
+        </a>
+    </li>
+</ul>
