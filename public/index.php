db778cf12877b73393d0f39264b18448aef955c9
Syed Haider Hassan <pakistanihaider@gmail.com>
2017-06-13 22:49:43 +0500

    test

--- public/index.php
+++ public/index.php
@@ -1,17 +1,10 @@
 <?php
-if(!function_exists('apache_get_modules') ){ phpinfo(); exit; }
-$res = 'Module Unavailable';
-if(in_array('mod_rewrite',apache_get_modules()))
-    $res = 'Module Available';
-?>
-<html>
-<head>
-    <title>A mod_rewrite availability check !</title></head>
-<body>
-<p><?php echo apache_get_version(),"</p><p>mod_rewrite $res"; ?></p>
-</body>
-</html>
-<?php
+ob_start();
+phpinfo(INFO_MODULES);
+$contents = ob_get_contents();
+ob_end_clean();
+var_dump(strpos($contents, 'mod_rewrite') !== false);
+
 exit;
 /**
  * Laravel - A PHP Framework For Web Artisans
