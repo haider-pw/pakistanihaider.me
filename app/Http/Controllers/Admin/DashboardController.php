c4a6d89c1737f7344618b1240e4c7efafda4ea6a
Syed Haider Hassan <pakistanihaider@gmail.com>
2017-06-14 07:57:53 +0500

    fixed a path for view.
    it was case sensitive and was not working on server.

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -20,7 +20,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('Admin.dashboard');
+        return view('admin.dashboard');
     }
 
 }
