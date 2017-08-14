11f2e8826e8230d87f29dbe7271acabd81c5eec3
Syed Haider Hassan <pakistanihaider@gmail.com>
2017-08-15 01:45:11 +0500

    Updated the Seeder File.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
          $this->call(ResumeTableSeeder::class);
          $this->call(SkillsGroupsTableSeeder::class);
          $this->call(SystemSkillsTableSeeder::class);
+         $this->call(ResumesBlocksVisibilitySeeder::class);
     }
 }
